64f44f8fe6740fb30b8c82b31cbc112caf1b000f
Alex Hughes <alex.hughes@digiflare.com>
2016-11-27 18:43:31 -0500

    task-1: Time-triggered Local Notification

--- NotificationSample/NotificationSample/ViewController.swift
+++ NotificationSample/NotificationSample/ViewController.swift
@@ -7,7 +7,76 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    // MARK: - Constants
+    
+    private struct Constants {
+        
+        struct Notifications {
+            
+            struct Identifier {
+                static let basic = "basicIdentifier"
+            }
+            
+        }
+    }
+    
+    // MARK: - Properties
+    
+    @IBOutlet private weak var titleTextField: UITextField!
 
+    @IBOutlet private weak var subtitleTextField: UITextField!
+    
+    @IBOutlet private weak var bodyTextView: UITextView!
+    
+    @IBOutlet weak var scheduleStepper: UIStepper!
+    
+    @IBOutlet private weak var scheduleLabel: UILabel!
+    
+    private let userNotificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
+    
+    // MARK: - Methods
+    
+    private func makeNotificationContent() -> UNNotificationContent {
+        
+        let content = UNMutableNotificationContent()
+        
+        content.title    = titleTextField.text ?? ""
+        content.subtitle = subtitleTextField.text ?? ""
+        content.body     = bodyTextView.text
+        content.sound    = UNNotificationSound.default()
+        
+        return content
+    }
+    
+    private func scheduleNotification() {
+        
+        // Schedule trigger based on time interval
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: scheduleStepper.value, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: Constants.Notifications.Identifier.basic,
+                                            content: makeNotificationContent(),
+                                            trigger: trigger)
+        
+        userNotificationCenter.add(request) { error in
+            if let error = error {
+                NSLog("Error scheduling notification request. Error: \(error.localizedDescription)")
+                return
+            }
+            NSLog("Notification scheduled!")
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction private func scheduleStepperValueChanged(_ sender: UIStepper) {
+        scheduleLabel.text = "\(sender.value)s"
+    }
+    
+    @IBAction private func submitButtonAction(_ sender: UIButton) {
+        scheduleNotification()
+    }
 }
